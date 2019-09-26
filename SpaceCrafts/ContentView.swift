dbb6b65eab2406a893c68e1961750271da5fc0d4
Christian Aranda <christian@wearemobilefirst.com>
2019-09-26 07:27:09 +0200

    Initial Commit

---
+++ SpaceCrafts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceCrafts
+//
+//  Created by Christian Aranda on 26/09/2019.
+//  Copyright © 2019 Christian Aranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
